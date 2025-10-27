526b8eb84905d0e98ac5420563257794c7fc1941
Natan <150294496+NAMERIO@users.noreply.github.com>
2025-10-27 15:04:57 -0500

    Updated style.css

--- main.js
+++ main.js
@@ -147,23 +147,6 @@ function updateParticles() {
     requestAnimationFrame(updateParticles);
 }
 
-// const cards = document.querySelectorAll('.project-card');
-
-cards.forEach(card => {
-    card.addEventListener('mousemove', (e) => {
-        const rect = card.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-        const centerX = rect.width / 2;
-        const centerY = rect.height / 2;
-        const rotateX = (y - centerY) / 10;
-        const rotateY = (centerX - x) / 10;
-        card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale3d(1.05, 1.05, 1.05)`;
-    });
-    card.addEventListener('mouseleave', () => {
-        card.style.transform = 'perspective(1000px) rotateX(0) rotateY(0) scale3d(1, 1, 1)';
-    });
-});
 
 window.addEventListener('resize', () => {
     canvas.width = window.innerWidth;
